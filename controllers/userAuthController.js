57cd10abba658e08f9ee56d1c8a6dc94096098b9
Gurkirat Singh Arora <gsa119@sfu.ca>
2023-07-31 15:57:19 +0530

    Added bcrypt in user signup and login route

--- controllers/userAuthController.js
+++ controllers/userAuthController.js
@@ -1,5 +1,6 @@
 const UserModel = require('../models/UserModel');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 const userSignup = async (req, res) => {
     const { email, name, password } = req.body;
     if (!email || !password || !name) {
@@ -9,7 +10,9 @@ const userSignup = async (req, res) => {
     if (existingUser) {
         res.status(404).json({ msg: "User already exists" });
     }
-    const user = await UserModel.create({ email, name, password });
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+    const user = await UserModel.create({ email, name, password: hashPassword });
     const token = jwt.sign({ userId: user._id }, process.env.JWT_USER_SECRET, {
         expiresIn: process.env.JWT_LIFETIME
     })
@@ -29,6 +32,10 @@ const userLogin = async (req, res) => {
     const user = await UserModel.findOne({ email });
     if (!user) {
         res.status(404).json({ msg: "User not found" });
+    }
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+        res.json("Invalid Credentials");
     }
     const token = jwt.sign({ userId: user._id }, process.env.JWT_USER_SECRET, {
         expiresIn: process.env.JWT_LIFETIME
