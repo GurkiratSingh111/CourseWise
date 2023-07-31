c73295608911c9287cddbd6eaeace8a384eb08c1
Gurkirat Singh Arora <gsa119@sfu.ca>
2023-07-31 15:36:23 +0530

    Added password encryption in admin route

--- controllers/adminAuthController.js
+++ controllers/adminAuthController.js
@@ -1,5 +1,6 @@
 const AdminModel = require('../models/AdminModel');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 const adminSignup = async (req, res) => {
     const { email, name, password } = req.body;
     if (!email || !password || !name) {
@@ -9,12 +10,14 @@ const adminSignup = async (req, res) => {
     if (existingAdmin) {
         res.status(404).json({ msg: "Admin already exists" });
     }
-    const admin = await AdminModel.create({ email, name, password });
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+    const admin = await AdminModel.create({ email, name, password: hashPassword });
     const token = jwt.sign({ adminId: admin._id }, process.env.JWT_ADMIN_SECRET, {
         expiresIn: process.env.JWT_LIFETIME
     })
     res.status(201).json({
-        user: {
+        admin: {
             email: email,
             name: name,
             token
@@ -30,11 +33,15 @@ const adminLogin = async (req, res) => {
     if (!admin) {
         res.status(404).json({ msg: "Admin not found" });
     }
+    const isPasswordCorrect = await bcrypt.compare(password, admin.password);
+    if (!isPasswordCorrect) {
+        res.json("Invalid Credentials");
+    }
     const token = jwt.sign({ adminId: admin._id }, process.env.JWT_ADMIN_SECRET, {
         expiresIn: process.env.JWT_LIFETIME
     })
     res.status(201).json({
-        user: {
+        admin: {
             email: admin.email,
             name: admin.name,
             token
