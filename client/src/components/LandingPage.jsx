c35e812757828e275bd598d3dfc59f89e0fb94af
Gurkirat Singh Arora <gsa119@sfu.ca>
2023-08-04 22:54:52 +0530

    added about section to landing page

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -12,68 +12,6 @@ const LandingPage = () => {
     const mdSize = useMediaQuery(theme.breakpoints.up('md'))
     const smSize = useMediaQuery(theme.breakpoints.up('sm'))
     return (
-        // <div style={{ overflowX: "hidden", display: "flex", flexWrap: "wrap", backgroundImage: `url(${landingImage})`, height: "100vh", width: "80vw", backgroundRepeat: "no-repeat", }}>
-        //     <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-        //         <div style={{ position: "fixed", right: "20px", top: "100px" }}>
-        //             <Typography variant='h4' style={{ color: "#0a0f6b" }}>Find your <span style={{ color: "#eb83a9" }}>perfect</span> course <br />& grow your <span style={{ color: "#eb83a9" }}>skills</span> </Typography>
-        //             <br />
-        //             <Button
-        //                 variant="outlined"
-        //                 sx={{
-        //                     bgcolor: "#0a0f6b",
-        //                     position: "fixed",
-        //                     right: "200px",
-        //                     mr: 2,
-        //                     color: 'white',
-        //                     borderColor: "white",
-        //                     ':hover': {
-        //                         bgcolor: "#0a0f6b",
-        //                         color: "white",
-        //                         borderColor: "white"
-        //                     }
-        //                 }}
-        //             >Get Started</Button>
-        //         </div>
-        //     </div>
-        // </div>
-        // <Grid container>
-        //     <Grid item md={6} sm={12}
-        //         style={{
-        //             background: "black", width: "100%",
-        //             height: "100vh", position: "fixed"
-        //         }}>
-        //         <Typography style={{ color: "white" }}>Welcome</Typography>
-        //     </Grid>
-        //     <Grid item md={6} sm={0}>
-        //         <div style={{ backgroundImage: `url(${homeImage})`, width: "100vw", height: "100vh", backgroundRepeat: 'no-repeat' }} />
-
-        //     </Grid>
-        // </Grid>
-        // <div style={{ display: "flex", position: "fixed" }}>
-        //     <div style={{ width: mdSize ? "50vw" : '100vw', height: "800px", background: "black", position: "fixed", top: "0px", left: "0px", margin: "0px", zIndex: 10 }}>
-        //         <Typography variant="h3" color="white" style={{ top: "150px", left: "70px", position: "fixed", fontWeight: 900, width: !mdSize && "100%" }}
-        //             sx={{ margin: "auto", width: '50%' }}
-        //         >Find your <span style={{ color: '#1fa95b' }}>perfect</span>
-        //             <br></br>course & grow <br></br>your <span style={{ color: '#1fa95b' }}>skill</span></Typography>
-        //         <Typography style={{ color: "#a7acad", position: "fixed", top: "335px", left: "63px", minWidth: "30%", maxWidth: "80%", overflow: 'none' }}>Unlock Your Potential: Discover a World of Knowledge and Expertise on Our<br></br> Cutting-Edge Course Selling App. Elevate Your Skills, Empower Your Journey<br></br> - Start Learning Today!</Typography>
-        //     </div>
-        //     <Hidden mdDown>
-        //         <div style={{ width: "50vw", height: "800px", backgroundImage: `url(${homeImage})`, position: "fixed", backgroundRepeat: "no-repeat", backgroundSize: "cover", left: "50%", top: "0px" }}></div>
-        //     </Hidden>
-        // </div>
-        // <div style={{ display: "flex", position: "fixed", width: "100%", height: "100%" }}>
-        //     <div style={{ width: "50%", height: "800px", background: "black", position: "fixed", top: "0px", left: "0px", margin: "0px", zIndex: 10, display: "flex", flexDirection: "column", alignItems: "flex-start", justifyContent: "center" }}>
-        //         <Typography variant="h3" color="white" style={{ fontWeight: 900, marginLeft: "70px", maxWidth: "80%", whiteSpace: "nowrap" }}>
-        //             Find your <span style={{ color: '#1fa95b' }}>perfect</span> course & grow your <span style={{ color: '#1fa95b' }}>skill</span>
-        //         </Typography>
-        //         <Typography style={{ color: "#a7acad", marginLeft: "63px", maxWidth: "80%", marginTop: "10px" }}>
-        //             Unlock Your Potential: Discover a World of Knowledge and Expertise on Our Cutting-Edge Course Selling App. Elevate Your Skills, Empower Your Journey - Start Learning Today!
-        //         </Typography>
-        //     </div>
-        //     <Hidden mdDown>
-        //         <div style={{ width: "50%", height: "800px", backgroundImage: `url(${homeImage})`, backgroundRepeat: "no-repeat", backgroundSize: "cover", left: "50%", top: "0px", position: "fixed", backgroundPosition: "center" }}></div>
-        //     </Hidden>
-        // </div>
         <div style={{ top: "63px", width: "100%", height: "100%", display: "flex", flexDirection: "column", left: "0px", overflowY: "scroll", overflowX: "hidden" }}>
             <div style={{ top: "63px", width: "100%", height: "700px", display: "flex" }}>
                 <div style={{ width: mdSize ? "50%" : "100%", backgroundColor: "black", color: "white", display: "flex", flexDirection: "column", fontFamily: "serif" }}>
@@ -96,7 +34,7 @@ const LandingPage = () => {
                 </Hidden>
             </div>
             <div style={{ width: "100%", height: "150px", backgroundColor: "white", top: "800px", display: "flex", flexDirection: "column", alignItems: "center" }}>
-                <h1 style={{ fontWeight: 900 }}>Our Sponsors</h1>
+                <h1 style={{ fontWeight: 900, fontFamily: "serif", fontSize: "xxx-large" }}>Our Sponsors</h1>
                 <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap", alignItems: "center", justifyContent: "center" }}>
                     <img src={amazonLogo} alt="" style={{ height: "4rem", width: "10%", margin: "1.3rem", marginRight: "3%", boxSizing: "border-box" }} />
                     <img src={appleLogo} alt="" style={{ height: "4rem", width: "10%", margin: "1.3rem", marginRight: "3%", boxSizing: "border-box" }} />
@@ -106,9 +44,33 @@ const LandingPage = () => {
                 </div>
             </div>
             <hr style={{ backgroundColor: "grey", height: "6px", width: "100vw", margin: " 100px 0px 0px" }} />
-            <div style={{ display: "flex", justifyContent: "center", margin: "20px" }}>
-                <Typography>Achieve Your Goals With CourseWise</Typography>
+            <div style={{ display: "flex", justifyContent: "center", margin: "20px", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+                <Typography variant="h4" style={{ fontWeight: 900, marginTop: "30px" }}>Achieve Your Goals With CourseWise</Typography>
+                <Typography variant='h6' style={{ width: "80%", color: "#7d8c95" }}>Explore, Learn, Excel: Your Gateway to Skillful Success! Discover a vast array of courses tailored to elevate your expertise.</Typography>
             </div>
+            <div style={{ display: "flex", flexDirection: mdSize ? "row" : "column", justifyContent: "center", flexWrap: "nowrap", alignItems: "center" }}>
+                <div style={{ width: mdSize ? "24%" : "80%", backgroundColor: "#b9f4dc", height: "15rem", marginRight: mdSize ? "2rem" : "3rem", marginLeft: mdSize && "3rem", boxShadow: "7px 7px 7px black", marginBottom: "3rem" }}>
+                    <div style={{ display: "flex", flexDirection: "column" }}>
+                        <h3 style={{ fontWeight: 900, marginLeft: "15px" }}>Learn latest skills</h3>
+                        <p style={{ marginLeft: "15px", marginTop: "0px" }}>Stay Ahead with the Latest Skills: Embrace the Future of Learning on Our Cutting-Edge Course Platform. Discover the Hottest Skills in Demand.</p>
+                    </div>
+                </div>
+                <div style={{ width: mdSize ? "24%" : "80%", backgroundColor: "#fef0ca", height: "15rem", marginRight: mdSize ? "2rem" : "3rem", boxShadow: " 7px 7px 7px black", marginBottom: "3rem" }}>
+                    <div style={{ display: "flex", flexDirection: "column" }}>
+                        <h4 style={{ fontWeight: 900, marginLeft: "15px" }}>Get ready for a career</h4>
+                        <p style={{ marginLeft: "15px", marginTop: "0px" }}>Prepare for Your Dream Career: Unlock Your Potential with Expert-Led Courses. Empower Yourself and Embrace the Path.</p>
+                    </div>
+                </div>
+                <div style={{ width: mdSize ? "24%" : "80%", backgroundColor: "#cac0e5", height: "15rem", marginRight: mdSize ? "2rem" : "3rem", boxShadow: "7px 7px 7px black", marginBottom: "3rem" }}>
+                    <div style={{ display: "flex", flexDirection: "column" }}>
+                        <h4 style={{ fontWeight: 900, marginLeft: "15px" }}>Earn a certificate</h4>
+                        <p style={{ marginLeft: "15px", marginTop: "0px" }}>Certify Your Expertise: Earn Recognized Certificates with Our Skill-Enriching Courses. Take Your Career to New Horizons.</p>
+                    </div>
+                </div>
+
+            </div>
+            <hr style={{ color: "grey", height: "9px", width: "100vw", marginTop: "20px" }} />
+            <div></div>
         </div>
     )
 }
