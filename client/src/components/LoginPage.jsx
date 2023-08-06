e9cf46be10cf1114b843dce2c154e6f813cf72d5
Gurkirat Singh Arora <gsa119@sfu.ca>
2023-08-06 10:26:36 +0530

    Added routing to signup and login

--- client/src/components/LoginPage.jsx
+++ client/src/components/LoginPage.jsx
@@ -1,12 +1,14 @@
 import { Button, Card, TextField, Typography } from '@mui/material'
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const LoginPage = () => {
+    const navigate = useNavigate();
     return (
         <div style={{ width: "100%", height: '100vh' }}>
             <div style={{ display: "flex", justifyContent: "space-between" }}>
                 <Typography variant="h4" style={{ fontFamily: "serif", fontWeight: 900, margin: "2rem" }}>CourseWise</Typography>
-                <Button variant="h5" style={{ fontWeight: 700, margin: "2rem", width: "6rem", border: "1px solid black", borderRadius: "14px" }}>SIGN UP</Button>
+                <Button variant="h5" onClick={() => { navigate('/signup') }} style={{ fontWeight: 700, margin: "2rem", width: "6rem", border: "1px solid black", borderRadius: "14px" }}>SIGN UP</Button>
             </div>
             <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
                 <Typography variant="h2" style={{ fontWeight: 900 }}>Welcome back!</Typography>
@@ -15,7 +17,7 @@ const LoginPage = () => {
                     <TextField style={{ width: "60%", marginTop: "3rem" }} id="outlined-email-input" label="Email" type="email" />
                     <TextField style={{ width: "60%", marginTop: "1rem", marginBottom: "0" }} idid="outlined-password-input" label="Password" type="password" />
                     <Button variant="h5" style={{ backgroundColor: "black", color: "white", fontWeight: 700, margin: "2rem", width: "6rem", border: "1px solid black", borderRadius: "14px" }}>LOG IN </Button>
-                    <span style={{ marginBottom: "2rem", color: "grey", fontWeight: 700 }}>Don't have an account ?<Button style={{ color: '#3b7ffc', fontWeight: 600 }}> Sign up for free</Button>
+                    <span style={{ marginBottom: "2rem", color: "grey", fontWeight: 700 }}>Don't have an account ?<Button onClick={() => { navigate('/signup') }} style={{ color: '#3b7ffc', fontWeight: 600 }}> Sign up for free</Button>
                     </span>
                 </Card>
             </div>
--- client/src/components/SignupPage.jsx
+++ client/src/components/SignupPage.jsx
@@ -1,12 +1,14 @@
 import { Button, Card, TextField, Typography } from '@mui/material'
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const SignupPage = () => {
+    const navigate = useNavigate();
     return (
         <div style={{ width: "100%", height: '100vh' }}>
             <div style={{ display: "flex", justifyContent: "space-between" }}>
                 <Typography variant="h4" style={{ fontFamily: "serif", fontWeight: 900, margin: "2rem" }}>CourseWise</Typography>
-                <Button variant="h5" style={{ fontWeight: 700, margin: "2rem", width: "6rem", border: "1px solid black", borderRadius: "14px" }}>Log In </Button>
+                <Button variant="h5" onClick={() => { navigate('/login') }} style={{ fontWeight: 700, margin: "2rem", width: "6rem", border: "1px solid black", borderRadius: "14px" }}>Log In </Button>
             </div>
             <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
                 <Typography variant="h2" style={{ fontWeight: 900 }}>Hi there!</Typography>
@@ -16,7 +18,7 @@ const SignupPage = () => {
                     <TextField style={{ width: "60%", marginTop: "1rem" }} id="outlined-email-input" label="Email" type="email" />
                     <TextField style={{ width: "60%", marginTop: "1rem", marginBottom: "0" }} id="outlined-password-input" label="Password" type="password" />
                     <Button variant="h5" style={{ backgroundColor: "black", color: "white", fontWeight: 700, margin: "2rem", width: "6rem", border: "1px solid black", borderRadius: "14px" }}>Sign up </Button>
-                    <span style={{ marginBottom: "2rem", fontWeight: 700, color: 'grey' }}>Already have an account ?<Button style={{ color: '#3b7ffc' }}> Login</Button>
+                    <span style={{ marginBottom: "2rem", fontWeight: 700, color: 'grey' }}>Already have an account ?<Button style={{ color: '#3b7ffc' }} onClick={() => { navigate('/login') }}> Login</Button>
                     </span>
                 </Card>
             </div>
