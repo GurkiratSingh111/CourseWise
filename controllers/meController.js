a51b5a475162a2f77305e1863c907a67a4739542
Gurkirat Singh Arora <gsa119@sfu.ca>
2023-08-09 11:49:28 +0530

    Completed me Route for both user and admin

--- controllers/meController.js
+++ controllers/meController.js
@@ -1,11 +1,21 @@
-const AdminModel = require('../models/AdminModel');
+const Admin = require('../models/AdminModel');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
-const UserModel = require('../models/UserModel');
+const User = require('../models/UserModel');
 
 
-const meRoute = (req, res) => {
-    res.send("This is me controller");
+const meRoute = async (req, res) => {
+    const user = await User.findOne({ _id: req.user.userId });
+    if (!user) {
+        const admin = await Admin.findOne({ _id: req.user.userId });
+        if (!admin) {
+            res.json("No user/admin exist");
+        }
+        const { name, email } = admin;
+        res.json({ name, email });
+    }
+    const { name, email } = user;
+    res.json({ name, email });
 }
 
 module.exports = {
