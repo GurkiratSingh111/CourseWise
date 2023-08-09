7129912103455d7c766a884e31195237fb3d8dce
Gurkirat Singh Arora <gsa119@sfu.ca>
2023-08-09 21:57:20 +0530

    CourseCard now shows image fetched from backend

--- client/src/components/CourseCard.jsx
+++ client/src/components/CourseCard.jsx
@@ -4,10 +4,10 @@ import homeImage from '../images/homeImage.png';
 import { useNavigate } from 'react-router-dom';
 const CourseCard = (props) => {
     const navigate = useNavigate();
-    const { name, price, description, _id } = props.course;
+    const { name, price, description, _id, image } = props.course;
     console.log(name);
     return (<Card onClick={() => { navigate(`/course/${_id}`) }} variant="elevation" sx={{ maxWidth: 354 }} style={{ marginTop: "1rem", marginLeft: "3rem" }}>
-        <CardMedia sx={{ height: 240 }} image={homeImage} title="course" />
+        <CardMedia sx={{ height: 240 }} image={image} title="course" />
         <CardContent style={{ display: "flex", justifyContent: "space-between" }}>
             <div style={{ display: "flex", flexDirection: "column", width: "70%" }}>
                 <Typography variant='subtitle1' style={{ fontWeight: 900 }}>{name}</Typography>
