b6037a5bb432695c20c4ce6fe478a95f2b8f3ec8
Gurkirat Singh Arora <gsa119@sfu.ca>
2023-08-12 01:07:41 +0530

    Added delete route on frontend

--- client/src/components/Course.jsx
+++ client/src/components/Course.jsx
@@ -1,15 +1,20 @@
 import { Box, Card, CircularProgress, Grid } from "@mui/material";
 import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { Typography, TextField, Button } from "@mui/material";
 import axios from "axios";
 import HomeImage from '../images/homeImage.png'
 import { courseState } from "../store/atoms/course";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { courseName, coursePrice, isCourseLoading, courseImage } from "../store/selector/course";
+import { userRoleState } from "../store/selector/userRole";
 
 
 function Course() {
+    const navigate = useNavigate();
+    const role = useRecoilValue(userRoleState);
+    const userRole = role === 'user';
+    const adminRole = role === 'admin';
     let { courseId } = useParams();
     const [courseDetails, setCourse] = useRecoilState(courseState);
     const [isLoading, setIsLoading] = useState(true);
@@ -33,19 +38,45 @@ function Course() {
             <CircularProgress size="20rem" sx={{ color: "black" }} />
         </Box>
     }
+    const updateCourse = () => {
+
+
+
+    }
+    const deleteCourse = async () => {
+        const response = await axios.delete(`http://localhost:4000/api/v1//courseadmin/${courseId}`, {
+            headers: {
+                "Authorization": "Bearer " + localStorage.getItem("token"),
+                'Content-Type': 'application/json'
+            }
+        })
+        console.log(response);
+        navigate('/admin/mycourses');
+    }
+
 
     return <div style={{ height: "100%", width: "100%", marginTop: "3.5rem" }}>
         <div style={{
             color: "white", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center",
-            backgroundImage: `url(${courseDetails.course.image})`, backgroundSize: 'cover', marginTop: "2rem", height: "300px", opacity: "1"
+            backgroundImage: `url(${courseDetails.course.image})`, filter: "brightness(80%)", backgroundSize: 'cover', marginTop: "2rem", height: "300px", opacity: "1"
         }}>
-            <Typography variant="h3" style={{ marginBottom: "1rem" }}>{courseDetails.course.name}</Typography>
-            <Typography variant="h6" style={{ color: "orange", marginBottom: "1rem" }}>$ {courseDetails.course.price}</Typography>
-            <Button variant="outlined" style={{ marginBottom: "1rem", borderColor: "orange", color: "orange" }} sx={{
+            <Typography variant="h3" style={{ marginBottom: "1rem", color: "black" }}>{courseDetails.course.name}</Typography>
+            <Typography variant="h6" style={{ color: "black", marginBottom: "1rem" }}>$ {courseDetails.course.price}</Typography>
+            {userRole && <Button variant="outlined" style={{ marginBottom: "1rem", borderColor: "black", color: "black" }} sx={{
                 '&:hover': {
                     backgroundColor: "#3c3c3b"
                 }
-            }}>Buy Course</Button>
+            }}>Buy Course</Button>}
+            {adminRole && <Button onClick={updateCourse} variant="outlined" style={{ marginBottom: "1rem", borderColor: "black", color: "black" }} sx={{
+                '&:hover': {
+                    backgroundColor: "#3c3c3b"
+                }
+            }}>Update Course</Button>}
+            {adminRole && <Button onClick={deleteCourse} variant="outlined" style={{ marginBottom: "1rem", borderColor: "black", color: "black" }} sx={{
+                '&:hover': {
+                    backgroundColor: "#3c3c3b"
+                }
+            }}>Delete Course</Button>}
         </div>
         <Typography variant="h5" style={{ marginTop: "3rem", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: 'center' }}>
             {courseDetails.course.description}
